7948d30261d75a49ffbf78b6fc1f40376c9b5c39
Salvador <sfandino@yahoo.com>
2011-06-21 18:49:29 +0200

    first working version

--- lib/Math/nSphere.pm
+++ lib/Math/nSphere.pm
@@ -1,84 +1,101 @@
 package Math::nSphere;
 
-use 5.012003;
-use strict;
-use warnings;
-
-require Exporter;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Math::nSphere ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
 our $VERSION = '0.01';
 
+use strict;
+use warnings;
+use Carp;
 
-# Preloaded methods go here.
+use Exporter qw(import);
+our @EXPORT_OK = qw(nsphere_surface nsphere_volumen);
+
+my $PI = 3.14159265358979323846264338327950288419716939937510;
+
+my @S = ( 0 , 2 * $PI , 4 * $PI );
+my @V = ( 0 ,       2 ,     $PI , 4 / 3 * $PI );
+
+sub nsphere_surface {
+    my ($n, $r) = @_;
+    my $S = $S[$n];
+    unless (defined $S) {
+        $n = int $n;
+        $n >= 0 or croak "index out of range";
+        my $i = $n;
+        $i -= 2 until defined $S[$i];
+        $S = $S[$i];
+        while ($i < $n) {
+            $i += 2;
+            $S[$i] = $S = 2 * $PI * $S / ($i - 1);
+        }
+    };
+    return $S unless defined $r;
+    $r ** $n * $S;
+}
+
+sub nsphere_volumen {
+    my ($n, $r) = @_;
+    my $V = $V[$n];
+    unless (defined $V) {
+        $n = int $n;
+        $n >= 0 or croak "index out of range";
+        my $i = $n;
+        $i -= 2 until defined $V[$i];
+        $V = $V[$i];
+        while ($i < $n) {
+            $i += 2;
+            $V[$i] = $V = 2 * $PI * $V / $i;
+        }
+    };
+    return $V unless defined $r;
+    $r ** ($n + 1) * $V;
+}
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-Math::nSphere - Perl extension for blah blah blah
+Math::nSphere - calculate volumen and surface of n-spheres
 
 =head1 SYNOPSIS
 
-  use Math::nSphere;
-  blah blah blah
+  use Math::nSphere qw(nsphere_surface nshepere_volumen);
+
+  my $sur = nsphere_surface($dim - 1, $radius);
+  my $vol = nsphere_volumen($dim - 1, $radius);
 
 =head1 DESCRIPTION
 
-Stub documentation for Math::nSphere, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+This module provides functions to calculate the surface and the
+volumen of n-spheres of any dimension.
 
-Blah blah blah.
+Note that n + 1 equals the space dimension. For instace, a
+circunference is a 1-sphere and a sphere is a 2-sphere.
 
-=head2 EXPORT
+=over 4
 
-None by default.
+=item $sur = nsphere_surface($n, $r)
 
+Returns the surface of the n-sphere of the given radius (1.0 by
+default).
 
+=item $vol = nsphere_volumen($n, $r)
+
+Returns the volumen of the n-sphere of the given radius (1.0 by
+default).
+
+=back
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
-
-=head1 AUTHOR
-
-Salvador Fandino, E<lt>salva@E<gt>
+n-sphere entry at the Wikipedia: L<http://en.wikipedia.org/wiki/N-sphere>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2011 by Salvador Fandino
+Copyright (C) 2011 by Salvador Fandino (sfandino@yahoo.com)
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.12.3 or,
 at your option, any later version of Perl 5 you may have available.
-
 
 =cut
